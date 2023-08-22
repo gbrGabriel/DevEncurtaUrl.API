00000c679216d04638883241e21a80598603819f
Gabriel Silva <gabrielgbr.contato@hotmail.com>
2023-08-22 10:18:59 -0300

    add new middleware in pipeline

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 using DevEncurtaUrl.API.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Serilog;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,19 +10,61 @@ builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "DevEncurtaUrl - API",
+        Description = "Api para encurtamento de links.",
+        Version = "v1",
+        Contact = new OpenApiContact
+        {
+            Name = "Gabriel Silva",
+            Email = "gabrielgbr.contato@gmail.com",
+            Url = new Uri("https://www.linkedin.com/in/gbrgabriel/")
+        }
+    });
 
-builder.Services.AddSingleton<DevEncurtaUrlDbContext>();
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+    opt.IncludeXmlComments(xmlPath);
+});
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<DevEncurtaUrlDbContext>(opt =>
+    opt.UseSqlServer(connectionString)
+);
+
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    Serilog.Log.Logger = new LoggerConfiguration()
+        .Enrich.FromLogContext()
+        .WriteTo.MSSqlServer(connectionString, "Logs", autoCreateSqlTable: true)
+        .WriteTo.Console()
+        .CreateLogger();
+}).UseSerilog();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+if (true)
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseAuthorization();
 
